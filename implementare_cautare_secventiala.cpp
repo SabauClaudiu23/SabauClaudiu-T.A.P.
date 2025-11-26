3d862d244445345ff623830e95b33b86e97e6ce9
Claudiu S <claudiusabau23@yahoo.com>
2025-11-26 12:28:55 +0200

    Add files via upload

---
+++ implementare_cautare_secventiala.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n, x;
+    cout << "Numarul de elemente: ";
+    cin >> n;
+
+    int v[100];
+    cout << "Elementele vectorului:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> v[i];
+    }
+
+    cout << "Valoarea cautata: ";
+    cin >> x;
+
+   
+    int poz = -1;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == x) {
+            poz = i;  
+            break;     
+        }
+    }
+
+    if (poz == -1)
+        cout << "Elementul nu a fost gasit.\n";
+    else
+        cout << "Element gasit pe pozitia " << poz << ".\n";
+
+    return 0;
+}
